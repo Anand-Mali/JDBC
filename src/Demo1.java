d398e5b618440031fc28b69a1544cc869297f0a0
anand <anandmali1890@gmail.com>
2023-01-27 14:59:57 +0530

    this is my first commit

--- src/Demo1.java
+++ src/Demo1.java
@@ -2,8 +2,13 @@
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String name = "Anand";
+		if(name == "Anand") {
+			System.out.println("Allow");
+		}else {
+			System.out.println("Not allow");
+		}
 	}
 
 }
